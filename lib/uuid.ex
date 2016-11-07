c2f7c33bb6b0e09c233a7cc4a2fbb45bdf068615
Dax Ravi <d@ironbay.digital>
2016-11-07 00:58:00 -0500

    UUID

---
+++ lib/uuid.ex
@@ -0,0 +1,45 @@
+defmodule Delta.UUID do
+	@base 63
+	@ascending "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz" |> String.split("") |> Enum.take(@base)
+	@descending Enum.reverse(@ascending)
+	@length 8
+	@total @length + 12
+
+	def descending() do
+		descending_from(:os.system_time(:milli_seconds))
+	end
+
+	def descending_from(time) do
+		generate(time, @descending)
+	end
+
+	def ascending() do
+		ascending_from(:os.system_time(:milli_seconds))
+	end
+
+	def ascending_from(time) do
+		generate(time, @ascending)
+	end
+
+	def generate(time, pool) do
+		generate(time, pool, @total, [])
+		|> Enum.join
+	end
+
+	# Random Part
+	def generate(time, pool, count, collect) when count > @length do
+		collect = [Enum.random(pool) | collect]
+		generate(time, pool, count - 1, collect)
+	end
+
+	# Time Part
+	def generate(time, pool, count, collect) when count > 0 do
+		n = rem(time, @base)
+		collect = [Enum.at(pool, n) | collect]
+		generate(div(time, @base), pool, count - 1, collect)
+	end
+
+	def generate(time, pool, count, collect) do
+		collect
+	end
+end
