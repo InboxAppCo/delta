510cdea10a635254a2bd61af7bdfbbe484bfd898
Dax Ravi <d@ironbay.digital>
2016-11-07 08:51:39 -0500

    Delta master

--- lib/plugins/query.ex
+++ lib/plugins/query.ex
@@ -1,9 +1,24 @@
 defmodule Delta.Plugin.Query do
 	defmacro __using__(_opts) do
+		use Delta.Base
 		alias Delta.Query
 
 		quote do
 			def query_path(path, opts \\ %{}) do
+				query_path("delta-master", path, opts)
+			end
+
+			def query_path(user, path, opts \\ %{}) do
+				case interceptors
+					|> Stream.map(&(&1.resolve_query(path, user, opts)))
+					|> Stream.filter(&(&1 !== nil))
+					|> Stream.take(1)
+					|> Enum.to_list
+				do
+					[result] -> result
+					[] ->
+						Query.path(read, path, opts)
+				end
 				Query.path(read, path, opts)
 			end
 
