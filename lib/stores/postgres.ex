8a618ec5c966cbf1ee95f3bb44a2c2f919cdb140
Dax Ravi <d@ironbay.digital>
2016-12-08 07:40:20 -0500

    Fixed multi insert

--- lib/stores/postgres.ex
+++ lib/stores/postgres.ex
@@ -10,15 +10,15 @@ defmodule Delta.Stores.Postgres do
 	def merge(state, atoms) do
 		{_, statement, params} =
 			atoms
-			|> Enum.reduce({1, "", []}, fn {path, value}, {index, statement, params} ->
+			|> Enum.reduce({1, [], []}, fn {path, value}, {index, statement, params} ->
 				{
 					index + 2,
-					statement <> "($#{index}, $#{index + 1})",
+					["($#{index}, $#{index + 1})" | statement],
 					[Enum.join(path, "."), Poison.encode!(value) | params],
 				}
 		end)
 		state
-		|> Postgrex.query!("INSERT INTO data(path, value) VALUES #{statement} ON CONFLICT (path) DO UPDATE SET value = excluded.value", params)
+		|> Postgrex.query!("INSERT INTO data(path, value) VALUES #{Enum.join(statement, ", ")} ON CONFLICT (path) DO UPDATE SET value = excluded.value", params)
 	end
 
 	def delete(_state, []) do
