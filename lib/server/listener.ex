0f8a2a04c2be44306b070faa278eb19885e91da0
Dax Ravi <d@ironbay.digital>
2017-03-28 21:48:13 -0400

    Listener

--- lib/server/listener.ex
+++ lib/server/listener.ex
@@ -8,13 +8,14 @@ defmodule Delta.Server.Listener do
 
 	defp loop(server) do
 		case server |> Web.accept do
-			{:ok, socket} ->
-				case socket |> Web.accept! do
+			{:ok, socket = %{headers: %{"connection" => "Upgrade"} } } ->
+				case Web.accept!(socket, []) do
 					_ ->
 						Delta.Supervisor.start_child(Delta.Server.Processor, [socket])
 						Delta.Supervisor.start_child(Delta.Server.Reader, [socket])
 					_ -> :skip
 				end
+			{:ok, socket} -> Web.close(socket)
 			_ -> :skip
 		end
 		loop(server)
