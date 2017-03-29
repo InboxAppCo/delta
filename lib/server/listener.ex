083bb98bf8d0fdf0ab3ab6eecfa381df004dc1fa
Dax Ravi <d@ironbay.digital>
2017-03-29 07:01:19 -0400

    Looser match

--- lib/server/listener.ex
+++ lib/server/listener.ex
@@ -8,14 +8,13 @@ defmodule Delta.Server.Listener do
 
 	defp loop(server) do
 		case server |> Web.accept do
-			{:ok, socket = %{headers: %{"Connection" => "Upgrade"} } } ->
+			{:ok, socket } ->
 				case Web.accept!(socket, []) do
 					_ ->
 						Delta.Supervisor.start_child(Delta.Server.Processor, [socket])
 						Delta.Supervisor.start_child(Delta.Server.Reader, [socket])
 					_ -> :skip
 				end
-			{:ok, socket} -> Web.close(socket)
 			_ -> :skip
 		end
 		loop(server)
